e29e9c4b1a0a3b2c2a8542dc8b4935c98720dbc1
moneim <ashkeef76@gmail.com>
2023-09-15 13:46:31 +0300

    first commit

---
+++ database/factories/VenueFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Venue>
+ */
+class VenueFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'name'=> fake()->unique()->sentence(3),
+            'address'=> fake()->unique()->sentence(4),
+            'map_url'=> fake()->unique()->url(),
+        ];
+    }
+}
---
+++ database/migrations/2023_09_15_084450_create_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Organizer;
+use App\Models\User;
+use App\Models\Venue;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Organizer::class);
+            $table->foreignIdFor(Venue::class);
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->string('image_url');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
